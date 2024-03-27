5bd8ecb93e183b419cff2d9f63a07084a08383ea
Isabela Reis <isabelareis.dev@gmail.com>
2024-03-27 19:43:32 -0300

    Service layer, component registration

---
+++ src/main/java/com/isabelareis/projeto/services/UserService.java
@@ -0,0 +1,26 @@
+package com.isabelareis.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isabelareis.projeto.entities.User;
+import com.isabelareis.projeto.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
