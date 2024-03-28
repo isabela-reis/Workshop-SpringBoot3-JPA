5da45386873a2dfffab43b9d97b3c7329a0b8be6
Isabela Reis <isabelareis.dev@gmail.com>
2024-03-28 12:02:40 -0300

    Category

---
+++ src/main/java/com/isabelareis/projeto/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.isabelareis.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isabelareis.projeto.entities.Category;
+import com.isabelareis.projeto.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
