c35cb29652c474c8901d439c0d9a251fac81cb2c
Isabela Reis <isabelareis.dev@gmail.com>
2024-03-29 12:14:02 -0300

    User insert

--- src/main/java/com/isabelareis/projeto/repositories/OrderItemRepository.java
+++ src/main/java/com/isabelareis/projeto/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.isabelareis.projeto.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.isabelareis.projeto.entities.OrderItem;
+import com.isabelareis.projeto.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository  extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
